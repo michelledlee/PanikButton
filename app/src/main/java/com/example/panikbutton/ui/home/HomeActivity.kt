6205d67a78e1215ad7fdbe22bbbcc99429347ea6
Michelle Lee <lee.mic@husky.neu.edu>
2021-06-22 17:36:34 -0400

    Updated permissions requests - dialog boxes, system requests

--- app/src/main/java/com/example/panikbutton/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/panikbutton/ui/home/HomeActivity.kt
@@ -30,7 +30,8 @@ import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.DatabaseReference
 import java.util.jar.Manifest
 
-const val REQUEST_CODE = 1
+const val SMS_CODE = 1
+const val GPS_CODE = 2
 
 
 class HomeActivity : AppCompatActivity(), ReadAndWriteSnippets {
@@ -45,49 +46,13 @@ class HomeActivity : AppCompatActivity(), ReadAndWriteSnippets {
         initializeDbRef()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Check permissions
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)  != PackageManager.PERMISSION_GRANTED||
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (shouldShowRequestPermissionRationale(android.Manifest.permission.SEND_SMS) || shouldShowRequestPermissionRationale(android.Manifest.permission.SEND_SMS)) {
-                showInContextUISMS()
-                showInContextUILocation()
-            } else {
-                requestPermissions(arrayOf(android.Manifest.permission.SEND_SMS, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
-            }
-        }
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 //        supportActionBar?.setDisplayShowHomeEnabled(true)
         val panikButton: View = findViewById(R.id.panic_button)
         panikButton.setOnClickListener {
-
-            sendSMS()
-//            lastLocation()
-//            // Check for SMS permissions when using this app
-//            when { // Ensure that permission has been granted
-//                ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED -> {
-//                    sendSMS()
-//                }
-//                shouldShowRequestPermissionRationale(android.Manifest.permission.SEND_SMS) -> { // Explain why SMS is needed
-//                    showInContextUISMS()
-//                }
-//                else -> { // Request SMS permissions from the user
-//                    requestPermissions(arrayOf(android.Manifest.permission.SEND_SMS), REQUEST_CODE)
-//                }
-//            }
-//
-//            // Check for location permissions when using this app
-//            when { // Ensure that permission has been granted
-//                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
-//                    lastLocation()
-//                }
-//                shouldShowRequestPermissionRationale(android.Manifest.permission.SEND_SMS) -> { // Explain why location is needed
-//                    showInContextUILocation()
-//                }
-//                else -> { // Request SMS permissions from the user
-//                    requestPermissions(arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION), REQUEST_CODE)
-//                }
-//            }
+            checkSMSPermission(SMS_CODE)
+            checkGPSPermission(GPS_CODE)
+//            sendSMS()
         }
     }
 
@@ -136,17 +101,36 @@ class HomeActivity : AppCompatActivity(), ReadAndWriteSnippets {
         startActivity(intent)
     }
 
+    /** Check permissions for SMS functionality **/
+    private fun checkSMSPermission(requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.SEND_SMS), requestCode)
+        } else {
+            Toast.makeText(this, "SMS already granted", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    /** Check permissions for SMS functionality **/
+    private fun checkGPSPermission(requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), requestCode)
+        } else {
+            Toast.makeText(this, "GPS already granted", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
     /** Displays dialog explaining why the SMS permissions is needed **/
     private fun showInContextUISMS() {
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.sms_permissions_layout)
+        dialog.show()
         val okayButton = dialog.findViewById<Button>(R.id.okay_sms_perm)
         okayButton.setOnClickListener() {
-            dialog.dismiss()
-        }
-        val cancelButton = dialog.findViewById<Button>(R.id.back_sms_perm)
-        cancelButton.setOnClickListener() {
             dialog.dismiss()
         }
     }
@@ -156,18 +140,16 @@ class HomeActivity : AppCompatActivity(), ReadAndWriteSnippets {
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.background_location_layout)
+        dialog.show()
         val okayButton = dialog.findViewById<Button>(R.id.okay_bg_perm)
         okayButton.setOnClickListener() {
-            dialog.dismiss()
-        }
-        val cancelButton = dialog.findViewById<Button>(R.id.back_bg_perm)
-        cancelButton.setOnClickListener() {
             dialog.dismiss()
         }
     }
 
     /** Sends an SMS to each contact in the user's contact list **/
     @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun sendSMS() {
         val sharedPref =
             this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
@@ -197,6 +179,7 @@ class HomeActivity : AppCompatActivity(), ReadAndWriteSnippets {
                     if (TextUtils.isDigitsOnly(phoneNumber)) {
                         smsManager.sendTextMessage(phoneNumber, null, smsMessage, null, null)
                         val verificationMsg: String = String.format(resources.getString(R.string.message_sent), contactName)
+                        Log.e("hi", smsMessage)
                         Toast.makeText(this, verificationMsg, Toast.LENGTH_SHORT).show()
                     } else {
                         val message : String = String.format(getString(R.string.invalid_phone_number), contactName)
@@ -208,59 +191,38 @@ class HomeActivity : AppCompatActivity(), ReadAndWriteSnippets {
         }
     }
 
-//    /** Get the users last known location **/
-//    @SuppressLint("MissingPermission")
-//    @RequiresApi(Build.VERSION_CODES.M)
-//    private fun lastLocation() {
-////        if (ActivityCompat.checkSelfPermission(
-////                this,
-////                android.Manifest.permission.ACCESS_FINE_LOCATION
-////            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-////                this,
-////                android.Manifest.permission.ACCESS_COARSE_LOCATION
-////            ) != PackageManager.PERMISSION_GRANTED
-////        ) {
-////            // TODO: Consider calling
-////            //    ActivityCompat#requestPermissions
-////            // here to request the missing permissions, and then overriding
-////            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-////            //                                          int[] grantResults)
-////            // to handle the case where the user grants the permission. See the documentation
-////            // for ActivityCompat#requestPermissions for more details.
-////            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION), REQUEST_CODE)
-////            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
-////            return
-////        }
-//        fusedLocationClient.lastLocation
-//            .addOnSuccessListener { location : Location? ->
-//                // Got last known location. In some rare situations this can be null.
-//                if (location != null) {
-//                    val coord = location.latitude.plus(location.longitude)
-//                    Log.e("coordinates:", coord.toString())
-//
-//                }
-//            }
-//    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action.
+                    sendSMS()
+                    Log.e("sms", "sent sms")
+                } else {
+                    // Explain to the user that the feature is unavailable
+                    showInContextUISMS()
+                    Log.e("sms", "explain sms")
+                }
+                return
+            }
+            2 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action
+                    Log.e("gps", "sent gps")
+                } else {
+                    // Explain to the user that the feature is unavailable
+                    showInContextUILocation()
+                    Log.e("gps", "explain gps")
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
 
-//    /** Sends an email to each contact in the user's contact list **/
-//    private fun sendEmail() {
-//        val sharedPref =
-//            this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-//        val defaultValue = getString(R.string.current_user_id )
-//        val userId = sharedPref.getString(getString(R.string.current_user_id), defaultValue)
-//        if (userId != null) {
-//            // Get list from firebase based on userID
-//            getContacts(userId) {
-//                for (item in it) {
-//                    // Iterate through and send text messages
-//                    val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-//                        "mailto", item.contactEmail, null))
-//                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.default_email_subject))
-//                    emailIntent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.default_text_message))
-//                    startActivity(Intent.createChooser(emailIntent, resources.getString(R.string.label_send_email)))
-//                }
-//            }
-//        }
-//
-//    }
 }
